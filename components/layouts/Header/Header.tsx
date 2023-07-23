7f26f0aad2ac27d7a713b27db29206909f830579
Trust <okpusjamin@gmail.com>
2023-07-24 00:34:24 +0100

    feat: add mobile toggle icon to header

--- components/layouts/Header/Header.tsx
+++ components/layouts/Header/Header.tsx
@@ -1,11 +1,15 @@
+"use client";
 import Image from "next/image";
 import logo from "@/public/logo.svg";
 import LinkComponent from "@/components/atoms/LinkComponent";
+import styles from "./page.module.css";
 import Link from "next/link";
+import { useState } from "react";
+import clsx from "clsx";
 const routes = [
   {
     name: "Home",
-    url: "/home",
+    url: "/",
   },
   {
     name: "Events",
@@ -14,6 +18,11 @@ const routes = [
 ];
 
 export function Header() {
+  const [open, setOpen] = useState(false);
+
+  const toggleIsOpen = () => {
+    setOpen(!open);
+  };
   return (
     <header className="bg-white w-full">
       <div className="container mx-auto">
@@ -21,7 +30,7 @@ export function Header() {
           <div>
             <Image src={logo} alt="TCA Logo" />
           </div>
-          <nav className="">
+          <nav className="hidden">
             <ul className="flex justify-center text-gray-500">
               {routes.map((route) => (
                 <li key={route.name} className="ml-6 text-sm">
@@ -37,6 +46,23 @@ export function Header() {
               </li>
             </ul>
           </nav>
+          <button onClick={toggleIsOpen} className="md:hidden">
+            <div
+              className={clsx(
+                "h-0.5 bg-black my-1 w-6 transition duration-300 ease-in-out",
+                open && "w-6 rotate-45 -translate-x-2 translate-y-1 ",
+              )}
+            />
+            <div
+              className={clsx("h-0.5 bg-black my-1 w-8", open && "hidden")}
+            />
+            <div
+              className={clsx(
+                "h-0.5 bg-black my-1 w-6 transition duration-300 ease-in-out",
+                open && "w-6 -rotate-45 -translate-x-2 -translate-y-0.5",
+              )}
+            />
+          </button>
         </div>
       </div>
     </header>
