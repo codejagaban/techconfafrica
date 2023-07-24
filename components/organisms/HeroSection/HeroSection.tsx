6071b8a56442cc69310ddc7a3ab09976b3959ef9
Trust <okpusjamin@gmail.com>
2023-07-24 18:24:09 +0100

    feat: add responsiveness

--- components/organisms/HeroSection/HeroSection.tsx
+++ components/organisms/HeroSection/HeroSection.tsx
@@ -2,20 +2,20 @@ import LinkComponent from "@/components/atoms/LinkComponent";
 
 export function HeroSection() {
   return (
-    <section className=" h-screen">
-      <div className="container mx-auto">
-        <div className="text-center translate-y-3/4 relative flex flex-row items-center">
-          <h1 className="text-7xl font-black leading-[6rem]">
+    <section className=" h-screen py-10">
+      <div className="container mx-auto px-6">
+        <div className="text-center lg:translate-y-3/4 translate-y-1/4 relative flex flex-col items-center">
+          <h1 className="md:text-7xl text-4xl font-black md:leading-[6rem] leading-relaxed">
             Organize
-            <span className="mx-2 text-gray-100">•</span>
+            <span className="mx-2 text-gray-100"> •</span>
             Participate
-            <span className="mx-2 text-gray-100">•</span>
+            <span className="mx-2 text-gray-100"> •</span>
             Attend
-            <span className="mx-2 text-gray-100">•</span>
+            <span className="mx-2 text-gray-100"> •</span>
             <br />
             Tech <span className="text-orange-500"> Events</span> in Africa
           </h1>
-          <div className="top-40 relative grid grid-cols-2  gap-10 w-fit">
+          <div className="top-40 relative grid md:grid-cols-2  gap-10 w-fit">
             <LinkComponent
               size="lg"
               label="Host an Event"
