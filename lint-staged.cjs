afb8c586281936dc99713a870d094b89c7fa8b84
Jamin <okpusjamin@gmail.com>
2023-12-20 16:26:05 +0000

    chore: test for lints

--- lint-staged.cjs
+++ lint-staged.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  "*.{ts,tsx,astro}": ["eslint --max-warnings=0 --fix",],
+  "*.{ts,tsx,astro}": ["eslint --max-warnings=0 --fix"],
   "*.{ts,tsx}": ["tsc --skipLibCheck --noEmit"],
   "*.{ts,tsx,json,css,js, astro}": [
     "prettier --write --plugin-search-dir=. --plugin=prettier-plugin-astro",
