c8f538b8beb796efc3b04a022e6df273300de163
codejagaban <okpusjamin@gmail.com>
2025-06-26 18:47:39 +0100

    chore: fix swiper

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://techconf.africa/",
-  trailingSlash: "always",
+  trailingSlash: "ignore",
   // Use to always append '/' at end of url
   integrations: [tailwind(), react(), sitemap()],
 });
